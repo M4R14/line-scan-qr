ac5894c5785b016c7ab500d7c7e487ed1d94ef9c
M4R14 <vachirawit.mark@gmail.com>
2020-10-27 14:28:19 +0700

    line/liff

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 
+import liff from '@line/liff';
+
+liff.init({ liffId: '1655180874-rpzb2gR5' });
+
+const sendAlertIfNotInClient = () => { console.log('sendAlertIfNotInClient') }
+const toggleQrCodeReader = () => { console.log('toggleQrCodeReader') }
+
+const scanQRCode = () => {
+  if (!liff.isInClient()) {
+    sendAlertIfNotInClient();
+  } else {
+    if (liff.scanCode) {
+        liff.scanCode().then(result => {
+            // e.g. result = { value: "Hello LIFF app!" }
+            const stringifiedResult = JSON.stringify(result);
+            document.getElementById('scanQrField').textContent = stringifiedResult;
+            toggleQrCodeReader();
+        }).catch(err => {
+            document.getElementById('scanQrField').textContent = "scanCode failed!";
+        });
+    }
+  }
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +41,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <button onClick={scanQRCode} >Scan QR Code</button>
+      <div id="scanQrField" ></div>
     </div>
   );
 }

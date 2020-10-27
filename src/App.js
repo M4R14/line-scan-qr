406b59faa28cee6c4514fce95b8a15839fa61fde
M4R14 <vachirawit.mark@gmail.com>
2020-10-27 16:18:26 +0700

    Read QR Code

--- src/App.js
+++ src/App.js
@@ -1,49 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
+import Button from '@material-ui/core/Button';
 import liff from '@line/liff';
 
-liff.init({ liffId: '1655180874-rpzb2gR5' });
+liff.init({ 
+  liffId: '1655180874-rpzb2gR5'
+});
 
 const sendAlertIfNotInClient = () => { console.log('sendAlertIfNotInClient') }
 const toggleQrCodeReader = () => { console.log('toggleQrCodeReader') }
 
-const scanQRCode = () => {
+const sendMessageButton = (Message) => {
   if (!liff.isInClient()) {
     sendAlertIfNotInClient();
   } else {
-    if (liff.scanCode) {
-        liff.scanCode().then(result => {
-            // e.g. result = { value: "Hello LIFF app!" }
-            const stringifiedResult = JSON.stringify(result);
-            document.getElementById('scanQrField').textContent = stringifiedResult;
-            toggleQrCodeReader();
-        }).catch(err => {
-            document.getElementById('scanQrField').textContent = "scanCode failed!";
-        });
-    }
+      liff.sendMessages([{
+          'type': 'text',
+          'text': Message
+      }]).then(function() {
+        liff.closeWindow();
+      }).catch(function(error) {
+          window.alert('Error sending message: ' + error);
+      });
   }
 }
 
 function App() {
+  const [QRtxt, setQRtxt] = useState('');
+  const [scanCount, setScanCount] = useState(0);
+
+  const scanQRCode = () => {
+    setScanCount(scanCount + 1)
+    if (!liff.isInClient()) {
+      sendAlertIfNotInClient();
+    } else {
+      if (liff.scanCode) {
+          liff.scanCode().then(result => {
+              // e.g. result = { value: "Hello LIFF app!" }
+              const stringifiedResult = JSON.stringify(result);
+              setQRtxt(result.value);
+              // document.getElementById('scanQrField').textContent = stringifiedResult;
+              toggleQrCodeReader();
+          }).catch(err => {
+              setQRtxt('')
+              // document.getElementById('scanQrField').textContent = "scanCode failed!";
+          });
+      }
+    }
+  }
+
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <button onClick={scanQRCode} >Scan QR Code</button>
-      <div id="scanQrField" ></div>
+      <main>
+        {/* <div id="scanQrField" ></div> */}
+        <Button variant="contained"  onClick={scanQRCode} >Scan QR Code</Button>
+        <h2 className="QRtxt" > {QRtxt} </h2>
+        { QRtxt && (
+          <Button 
+            variant="contained" 
+            color="primary" 
+            onClick={() => sendMessageButton(QRtxt)}> 
+            Send Message
+          </Button>
+        )}
+      </main>
     </div>
   );
 }

232fbfdb96b80500a364272ced97d0d251081f40
M4R14 <vachirawit.mark@gmail.com>
2020-10-28 20:02:51 +0700

    BG

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import './App.css';
 import {useState} from 'react';
 
+import bgStyle from './bg.modules.scss';
+import BG_AB from './images/AB.png';
+import BG_A from './images/A.png';
+import BG_B from './images/B.png';
+import BG_O from './images/O.png';
+import Img1 from './images/img.png';
+
 import Button from '@material-ui/core/Button';
 import liff from '@line/liff';
 
@@ -53,17 +60,52 @@ function App() {
   return (
     <div className="App">
       <main>
-        {/* <div id="scanQrField" ></div> */}
-        <Button variant="contained"  onClick={scanQRCode} >Scan QR Code</Button>
-        <h2 className="QRtxt" > {QRtxt} </h2>
-        { QRtxt && (
-          <Button 
-            variant="contained" 
-            color="primary" 
-            onClick={() => sendMessageButton(QRtxt)}> 
-            Send Message
-          </Button>
-        )}
+        <div className="bg" >
+          <img className="bg-AB" src={BG_AB} ></img>
+          <img className="bg-B" src={BG_B} ></img>
+          <img className="bg-A" src={BG_A} ></img>
+          <img className="bg-O" src={BG_O} ></img>
+        </div>
+        <div className="control" >
+          <div className="control-main">
+            <img style={{gridRowStart: 1, gridRowEnd: 3, width: '100%', }} src={Img1} ></img>
+            <label style={{ 
+              alignSelf:"end",
+              justifySelf: "center",
+              fontSize: "1.2rem",
+            }} > 
+               { QRtxt ? QRtxt : "กดปุ่มเพื่อไปหน้า" }
+            </label>
+            <label style={{
+              fontWeight: "bold",
+              fontSize: "1.3rem",
+            }}  >  { QRtxt ? 'เลขที่' : "แสกนคิวอาร์โค้ด" } </label>
+            <div  className="button-box button-scan" style={{
+              ...(QRtxt ? {} : {
+                gridColumnStart: 1,
+                gridColumnEnd: 3,
+              }),
+              paddingTop: '0.5em',
+              paddingBottom: '0.5em',
+            }} >
+              <Button  
+                variant="contained" 
+                onClick={scanQRCode} 
+              >สกนคิว อาร์โค้ด</Button>
+            </div>
+            { QRtxt && (
+              <div  className="button-box" >
+                <Button 
+                  variant="contained" 
+                  color="primary" 
+                  className="button-scan"
+                  onClick={() => sendMessageButton(QRtxt)}> 
+                  Send Message
+                </Button>
+              </div>
+            )}
+          </div>
+        </div>
       </main>
     </div>
   );
